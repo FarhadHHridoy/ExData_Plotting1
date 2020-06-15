9a45cbf19bc922e828fcb44b3a37238086447ef5
Md. Farhad Hossain <37260907+FarhadHHridoy@users.noreply.github.com>
2020-06-15 14:29:30 +0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -20,7 +20,9 @@ with(subdata,{
   plot(DateTime,Sub_metering_1,type = "l",xlab = "",ylab = "Energy sub metering")
   points(DateTime,Sub_metering_2,type = "l",col = "red")
   points(DateTime,Sub_metering_3,type = "l",col = "blue")
-  legend("topright",col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd = "1",bty = "n")
+  legend("topright",col = c("black","red","blue"),
+         legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         lty=c(1,1,1), bty="n", cex=.5)
   
   plot(DateTime,Voltage,xlab = "datetime",ylab = "Voltage",type = "l")
   
